0c431d75b144d44a4db69a75f353db5ef371dbf1
Samantha Withanage <samantha@frostdigital.se>
2021-09-04 17:28:35 +0530

    Add guesture handler

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React, { ReactNode } from "react";
 import { StatusBar } from "react-native";
+// import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootNavigator from "./src/navigation/RootNavigator";
 
 function App(): ReactNode {
   return (
+    // <GestureHandlerRootView>
     <SafeAreaProvider>
       <StatusBar barStyle={"dark-content"} />
       <RootNavigator />
     </SafeAreaProvider>
+    // </GestureHandlerRootView>
   );
 }
 
